8d3c66ad57ed045fae4cdadebb9a503257ba5888
Christopher <christopherballenger@gmail.com>
2018-07-15 22:07:49 -0400

    Made some background changes.

--- folder.php
+++ folder.php
@@ -3,10 +3,12 @@
   include_once $_SERVER['DOCUMENT_ROOT'] . "/portapage/web-assets/tpl/app_header.php";
   include_once $_SERVER['DOCUMENT_ROOT'] . "/portapage/web-assets/tpl/app_nav.php";
 
+  $user_id = $_SESSION['user_id'];
   $folder_id = $_REQUEST['folderid'];
+  $folder_name = $_REQUEST['foldername'];
 
   $pictures_sql = <<<SQL
-    SELECT * FROM pictures WHERE folder_id = $folder_id;
+    SELECT * FROM pictures WHERE folder_id = $folder_id and user_id = $user_id;
 SQL;
 
   $pictures_result = $conn->query($pictures_sql);
@@ -15,11 +17,13 @@ SQL;
     if ($pictures_result->num_rows > 0) {
       while ($row = $pictures_result->fetch_assoc()) {
         $image = base64_encode($row['picture']);
+        $image_id = $row['picture_id'];
         $thumbnail = "
           <div class='row'>
             <div class='col-xs-6 col-md-3'>
-              <a href='#' class='thumbnail'></a>
-              <img src='data:image/jpeg;base64,$image' alt='". $row['name'] ."' style='max-width: 500px'; border-radius: 50%;'>
+              <a href='/portapage/picture.php?pictureid=$image_id&folderid=$folder_id&foldername=$folder_name' class='thumbnail'>
+                <img src='data:image/jpeg;base64,$image' alt='". $row['name'] ."' style='max-width: 500px'; border-radius: 50%;'>
+              </a>
             </div>" .
             $row['name']
           . "</div><br>
