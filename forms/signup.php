a18c593305b7ca3863da51eff1c4d37d2862fe6d
Christopher <christopherballenger@gmail.com>
2018-07-18 22:10:45 -0400

    Added the form-control class to the text inputs.

--- forms/signup.php
+++ forms/signup.php
@@ -16,12 +16,12 @@
         <!-- Email -->
         <div class="form-group row">
           <label for="email" class="col-md-2">Email</label>
-          <input type="email" name="email_id" id="email" class="col-md-10">
+          <input type="email" name="email_id" id="email" class="form-control col-md-10">
         </div>
         <!-- Password -->
         <div class="form-group row">
           <label for="pw" class="col-md-2">Password</label>
-          <input type="password" name="pw" id="pw" class="col-md-10">
+          <input type="password" name="pw" id="pw" class="form-control col-md-10">
         </div>
         <!-- Submit -->
         <div class="form-group row">
