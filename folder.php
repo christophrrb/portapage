ef0a854deb46b5cc2384a526ef639371e489327d
Christopher <christopherballenger@gmail.com>
2018-07-18 22:09:11 -0400

    Made  the folder's name.

--- folder.php
+++ folder.php
@@ -1,11 +1,11 @@
 <?php
-  $page_title = "Upload Image";
+  $folder_name = $_REQUEST['foldername'];
+  $page_title = $folder_name;
   include_once $_SERVER['DOCUMENT_ROOT'] . "/portapage/web-assets/tpl/app_header.php";
   include_once $_SERVER['DOCUMENT_ROOT'] . "/portapage/web-assets/tpl/app_nav.php";
 
   $user_id = $_SESSION['user_id'];
   $folder_id = $_REQUEST['folderid'];
-  $folder_name = $_REQUEST['foldername'];
 
   $pictures_sql = <<<SQL
     SELECT * FROM pictures WHERE folder_id = $folder_id and user_id = $user_id;
@@ -19,11 +19,21 @@ SQL;
         $image = base64_encode($row['picture']);
         $image_name = $row['name'];
         $image_id = $row['picture_id'];
+        $exif = exif_read_data("data://image/jpeg;base64," . base64_encode($row['picture']));
+        // echo $exif['Orientation'];
+
+        // Image Orientation from EXIF Data
+        if ($exif['Orientation'] == 6) {
+          $orientation = "90deg";
+        } else {
+          $orientation = "0deg";
+        }
+
         $thumbnail = "
-          <div class='row'>
+          <div class='row' style='padding: 50px;'>
             <div class='col-xs-6 col-md-3'>
               <a href='/portapage/picture.php?pictureid=$image_id&folderid=$folder_id&foldername=$folder_name' class='thumbnail'>
-                <img src='data:image/jpeg;base64,$image' alt='". $row['name'] ."' style='max-width: 500px'; border-radius: 50%;'>
+                <img src='data:image/jpeg;base64,$image' alt='". $row['name'] ."' style='max-width: 500px; border-radius: 5%; transform: rotate($orientation);'>
               </a>
               $image_name <br>
               <a href='index.php?action=deleteimage&pictureid=$image_id&folderid=$folder_id&foldername=$folder_name'>Delete</a>
